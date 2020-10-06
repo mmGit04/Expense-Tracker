c86eca619709277e22bc60197ca233c9e2c156f1
Mina <m.milosavljevic@levi9.com>
2020-10-06 23:49:05 +0200

    Implement Home Top bar info update.

--- ExpenseTracker/ExpenseTracker/Controller/TransactionsVC.swift
+++ ExpenseTracker/ExpenseTracker/Controller/TransactionsVC.swift
@@ -7,32 +7,60 @@
 //
 
 import UIKit
+import CoreData
 
 class TransactionsVC: UIViewController {
-
+    
     // Outlets
     
     @IBOutlet weak var currentBalanceLbl: UILabel!
     @IBOutlet weak var incomeLbl: UILabel!
     @IBOutlet weak var expenseLbl: UILabel!
     
+    var transactions: [Transaction] = []
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        setupTopBarInfo()
     }
-    */
-
+    
+    
+    func setupTopBarInfo() {
+        fetchTransactions()
+        var income = 0.0
+        var expense = 0.0
+        for t in transactions {
+            if t.type == TransactionType.income.rawValue {
+                income += t.amount
+            } else {
+                expense += t.amount
+            }
+        }
+        incomeLbl.text = "$" + String(format: "%.2f", income)
+        expenseLbl.text = "$" + String(format: "%.2f", expense)
+        currentBalanceLbl.text = String(format: "%.2f", income - expense)
+    }
+    
+    
+    func fetchTransactions() {
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<Transaction>(entityName: "Transaction")
+        
+        do {
+            transactions = try managedContext.fetch(fetchRequest)
+            print("Successfully fetched data.")
+        } catch {
+            debugPrint("Could not fetch: \(error.localizedDescription)")
+        }
+    }
 }
