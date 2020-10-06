46cc167080bea5ad7e99abefe470ae19918e941f
Mina <m.milosavljevic@levi9.com>
2020-10-06 22:47:48 +0200

    Setup TransactionCore Data Entity and implement save func.

--- ExpenseTracker/ExpenseTracker/Controller/AddItemVC.swift
+++ ExpenseTracker/ExpenseTracker/Controller/AddItemVC.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import CoreData
 
 class AddItemVC: UIViewController {
-
+    
+    // Outlets
+    
+    @IBOutlet weak var typeSegControl: UISegmentedControl!
+    @IBOutlet weak var noteTxtField: UITextField!
+    @IBOutlet weak var amountTxtField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("Documents Directory: ", FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last ?? "Not Found!")
+
     }
-
-
+    
+    
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+        save()
+        
+    }
+    
+    func save() {
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let transaction = Transaction(context: managedContext)
+        
+        guard let value = amountTxtField.text, amountTxtField.text != nil else  {
+            return
+        }
+        transaction.amount = Double(value)!
+        transaction.note = noteTxtField.text
+        if typeSegControl.selectedSegmentIndex == 0 {
+            transaction.type = TransactionType.income.rawValue
+        } else {
+            transaction.type = TransactionType.expense.rawValue
+        }
+        do {
+            try managedContext.save()
+            print("Succcesfully saved data.")
+        } catch {
+            debugPrint("Could not save transaction.")
+        }
+        
+    }
 }
 
---
+++ ExpenseTracker/ExpenseTracker/Enum/TransactionType.swift
@@ -0,0 +1,14 @@
+//
+//  TransactionType.swift
+//  ExpenseTracker
+//
+//  Created by Mina Milosavljevic on 10/6/20.
+//  Copyright © 2020 Mina Milosavljevic. All rights reserved.
+//
+
+import Foundation
+
+enum TransactionType: String {
+    case income
+    case expense
+}
