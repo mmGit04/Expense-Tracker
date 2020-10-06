6cf004ea5530fd2042b36627b9ee8d6f9a0d183a
Mina <m.milosavljevic@levi9.com>
2020-10-06 20:45:35 +0200

    Setup Tab Bar VC

---
+++ ExpenseTracker/ExpenseTracker/Controller/AddItemVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExpenseTracker
+//
+//  Created by Mina Milosavljevic on 10/6/20.
+//  Copyright © 2020 Mina Milosavljevic. All rights reserved.
+//
+
+import UIKit
+
+class AddItemVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ExpenseTracker/ExpenseTracker/Controller/TransactionsVC.swift
@@ -0,0 +1,30 @@
+//
+//  TransactionsVC.swift
+//  ExpenseTracker
+//
+//  Created by Mina Milosavljevic on 10/6/20.
+//  Copyright © 2020 Mina Milosavljevic. All rights reserved.
+//
+
+import UIKit
+
+class TransactionsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
