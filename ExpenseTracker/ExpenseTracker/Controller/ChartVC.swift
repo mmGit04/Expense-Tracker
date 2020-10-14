fe51bda5616de757a470267ea2a07bdab555020c
Mina <m.milosavljevic@levi9.com>
2020-10-14 16:17:12 +0200

    Implemented Pie chart for amount values

--- ExpenseTracker/ExpenseTracker/Controller/ChartVC.swift
+++ ExpenseTracker/ExpenseTracker/Controller/ChartVC.swift
@@ -7,24 +7,117 @@
 //
 
 import UIKit
+import Charts
+import CoreData
 
 class ChartVC: UIViewController {
-
+    
+    // Outlets
+    @IBOutlet weak var pieChartView: PieChartView!
+    
+    // Variables
+    var categoryTransactions: [Category: [Transaction]] = [:]
+    var startOfCurrentMonth: Date {
+        let startDate = Date()
+        let components = NSCalendar.current.dateComponents([.year, .month], from: startDate)
+        let startOfMonth = NSCalendar.current.date(from: components)!
+        return startOfMonth
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        fetchTransactionsByCategory()
+        setupChartData()
     }
-    */
+    
+    
+    func setupChartData() {
+        var dataEntries: [ChartDataEntry] = []
+        for category in categoryTransactions.keys {
+            let sum = sumOfTransactionsByCategory(transactions: categoryTransactions[category]!)
+            if sum != 0.0 {
+                let dataEntry = PieChartDataEntry(value: sum, label: category.title, data: category.title as AnyObject)
+             dataEntries.append(dataEntry)
+            }
+        }
+        // 2. Set ChartDataSet
+        let pieChartDataSet = PieChartDataSet(entries: dataEntries, label: nil)
+        pieChartDataSet.colors = generateRandomColors(numbersOfColor: categoryTransactions.count)
+        // 3. Set ChartData
+        let pieChartData = PieChartData(dataSet: pieChartDataSet)
 
+        pieChartView.data = pieChartData
+        print("Success")
+    }
+    
+    private func generateRandomColors(numbersOfColor: Int) -> [UIColor] {
+        var colors: [UIColor] = []
+        for _ in 0..<numbersOfColor {
+           let red = Double(arc4random_uniform(256))
+           let green = Double(arc4random_uniform(256))
+           let blue = Double(arc4random_uniform(256))
+           let color = UIColor(red: CGFloat(red/255), green:   CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+           colors.append(color)
+        }
+        return colors
+    }
+    
+    func sumOfTransactionsByCategory(transactions: [Transaction]) -> Double {
+        var sum = 0.0
+        for transaction in transactions {
+            if transaction.type == TransactionType.income.rawValue {
+                sum += transaction.amount
+            }
+        }
+        return sum
+    }
+    
+    // MARK: Core Data methods
+    private func fetchTransactionsByCategory() {
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<Transaction>(entityName: "Transaction")
+        
+        
+        // Define endOfMonth
+        var comps2 = DateComponents()
+        comps2.month = 1
+        comps2.day = -1
+        let endOfMonth = NSCalendar.current.date(byAdding: comps2, to: startOfCurrentMonth)!
+        
+        
+        let datePredicate = NSPredicate(format: "date >=%@ && date <= %@", startOfCurrentMonth as NSDate, endOfMonth as NSDate )
+        
+        fetchRequest.predicate = datePredicate
+        do {
+            let transactions = try managedContext.fetch(fetchRequest)
+            sortTransactionByCategory(for: transactions)
+            print("Successfully fetched data.")
+        } catch {
+            debugPrint("Could not fetch: \(error.localizedDescription)")
+        }
+    }
+    
+    func sortTransactionByCategory(for transactions: [Transaction]) {
+        
+        // Setup categoryTransactions
+        for trans in transactions {
+            let category = trans.categoryId
+            if let cat = category {
+                if categoryTransactions[cat] != nil {
+                    categoryTransactions[cat]?.append(contentsOf: [trans])
+                } else {
+                    categoryTransactions[cat] = [trans]
+                }
+            }
+        }
+    }
+    
 }
